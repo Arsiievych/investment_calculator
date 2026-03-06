c271cdb4365c0f3d168a0121fee41f153fab7390
Pavlo Arsiievych <pavlo.arsiievych@onix-systems.com>
2026-03-06 16:50:31 +0200

    add empty-results component: scaffold initial structure with HTML, CSS, and TypeScript files

---
+++ src/app/empty-results/empty-results.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-results',
+  imports: [],
+  templateUrl: './empty-results.html',
+  styleUrl: './empty-results.css',
+})
+export class EmptyResults {
+
+}
