f20ab43e8ec9a077603c9fe79079d6e62c016278
Pavlo Arsiievych <pavlo.arsiievych@onix-systems.com>
2026-03-06 14:21:06 +0200

    integrate investment-results component into app layout with initial styles and table implementation

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from "./header/header";
 import {InvestmentInputs} from "./investment-inputs/investment-inputs";
+import {InvestmentResults} from "./investment-results/investment-results";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, InvestmentInputs],
+  imports: [RouterOutlet, Header, InvestmentInputs, InvestmentResults],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
