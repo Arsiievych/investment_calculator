3769ee43ef1940d9afc41368e1e745a432fff2a5
Pavlo Arsiievych <pavlo.arsiievych@onix-systems.com>
2026-03-06 13:57:41 +0200

    add investment-inputs component

---
+++ src/app/investment-inputs/investment-inputs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-inputs',
+  imports: [],
+  templateUrl: './investment-inputs.html',
+  styleUrl: './investment-inputs.css',
+})
+export class InvestmentInputs {
+
+}
