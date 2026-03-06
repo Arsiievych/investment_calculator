54714f32783a5ac01a667fb835aec256d03a8fa7
Pavlo Arsiievych <pavlo.arsiievych@onix-systems.com>
2026-03-06 15:15:08 +0200

    enhance investment-inputs component: bind form fields with ngModel, add (ngSubmit) handler, and define default values in the class

--- src/app/investment-inputs/investment-inputs.ts
+++ src/app/investment-inputs/investment-inputs.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 
 @Component({
-  selector: 'app-investment-inputs',
-  imports: [FormsModule],
-  templateUrl: './investment-inputs.html',
-  styleUrl: './investment-inputs.css',
+    selector: 'app-investment-inputs',
+    imports: [FormsModule],
+    templateUrl: './investment-inputs.html',
+    styleUrl: './investment-inputs.css',
 })
 export class InvestmentInputs {
+    initialInvestment = 0;
+    annualInvestment = 0;
+    expectedReturn = 0;
+    duration = 0;
 
+    onSubmit() {
+        console.log(this.initialInvestment, this.annualInvestment, this.expectedReturn, this.duration);
+    }
 }
