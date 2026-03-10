9aef93b884350fa8589cebf96d739c179d564272
Pavlo Arsiievych <pavlo.arsiievych@onix-systems.com>
2026-03-10 17:55:04 +0200

    format investment-results: use Angular CurrencyPipe for consistent currency formatting in table rows

--- src/app/investment-results/investment-results.ts
+++ src/app/investment-results/investment-results.ts
@@ -1,9 +1,12 @@
 import {Component, Input} from '@angular/core';
 import {InvestmentOutputModelArray} from "./investment-output.model";
+import {CurrencyPipe} from "@angular/common";
 
 @Component({
   selector: 'app-investment-results',
-  imports: [],
+  imports: [
+    CurrencyPipe
+  ],
   templateUrl: './investment-results.html',
   styleUrl: './investment-results.css',
 })
